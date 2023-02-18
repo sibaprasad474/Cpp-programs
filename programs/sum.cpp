f5c119f45b6d5454b025e68abe0baa49c7c38980
sibaprasad474 <msibaprasad32@.com>
2023-02-18 11:43:35 +0530

    add all

--- programs/sum.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-    int a,b,sum;
-    cout<<"enter two numbers"<<endl;
-    cin>>a>>b;
-    sum=a+b;
-    cout<<"sum of " <<a <<" and "<<b<<" is "<<sum;
-}
